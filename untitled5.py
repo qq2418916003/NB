15ed763e96ca970287952e79772cd98f5beac956
qq2418916003 <34418304+qq2418916003@users.noreply.github.com>
2017-12-10 22:10:29 +0800

    Add files via upload

---
+++ untitled5.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  9 20:40:33 2017
+
+@author: NB
+"""
+
+import re
+import urllib.request
+url='http://maoyan.com/board/4?offset=0'
+re1=urllib.request.urlopen(url)
+html=re1.read()
+pattern=re.compile('<dd>.*?name"><a.*?">(.*?)</a>.*?star">(.*?)</p>.*?</dd>',re.S)
+html=html.decode('utf-8')
+items=re.findall(pattern,html)
+for item in items:
+    xy = {}
+    xy['名称'] = item[0]
+    xy[''] = item[1]
+    print(xy)
